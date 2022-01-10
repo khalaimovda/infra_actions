50be2ca7aaa1d7142a06df1be8e88697ffba0843
khalaimovda <halaimov_15@mail.ru>
2022-01-10 21:01:21 +0300

    created setup.cfg

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,3 +1,5 @@
+from django.urls import include, path
+
 """infra_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,8 +15,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
